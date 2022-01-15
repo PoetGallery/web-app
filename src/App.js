b0770d40d935b1e7becb1bfbb727d5b048051b0e
bengyles <bengyles@hotmail.com>
2022-01-15 01:39:59 +0100

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import {Box, Grommet} from 'grommet';
+import {Grommet} from 'grommet';
 import theme from './theme';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Test from './pages/Test';
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
-    <Grommet theme={theme}>
-      <Box align="center" alignContent="center" alignSelf="center" pad="large">
-      <h1>Poet Gallery</h1>
-    </Box>
-    </Grommet>
+      <Grommet theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/test" element={<Test/>}/>
+            <Route path="*" element={<NoPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </Grommet>
   );
 }
 
---
+++ src/pages/Test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Test() {
+  return (
+        <h1>Testpage</h1>
+  );
+}
+
+export default Test;
