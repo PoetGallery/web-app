2666f21f11c87a368438a3fd20ae956e82a462bb
Milena Monova <mimonova13@gmail.com>
2022-01-15 18:57:23 +0200

    textile replaced with nft.storage

--- src/api/textile.api.js
+++ src/api/textile.api.js
@@ -1,37 +1,15 @@
-import { Buckets } from '@textile/hub'
+import { NFTStorage, File } from 'nft.storage'
+
+const client = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE_KEY })
 
 
-const keyInfo = {
-  key: process.env.REACT_APP_TEXTILE_KEY, 
-  secret: process.env.REACT_APP_TEXTILE_SECRET
-}
-
-const bucketName = process.env.REACT_APP_BUCKET_NAME;
-
-var jsonToArray = function(json)
-{
-	var str = JSON.stringify(json, null, 0);
-	var ret = new Uint8Array(str.length);
-	for (var i = 0; i < str.length; i++) {
-		ret[i] = str.charCodeAt(i);
-	}
-	return ret
-};
-
-export async function pushJSONDocument  (json) {
-  const buckets = await Buckets.withKeyInfo(keyInfo)
-  const { root } = await buckets.getOrCreate(bucketName)
-  if (!root) throw new Error('bucket not created')
-  const buf = jsonToArray(json)
-  const path = `metadata.json`
-  const links = await buckets.pushPath(root.key, path, buf)
-  return `https://hub.textile.io${links.path.path}`;
-}
-
-export async function pushImage(content, path) {
-  const buckets = await Buckets.withKeyInfo(keyInfo);
-  const { root } = await buckets.getOrCreate(bucketName)
-  if (!root) throw new Error('bucket not created')
-  const links = await buckets.pushPath(root.key, path, content)
-  return `https://hub.textile.io${links.path.path}`;
+// image should be byte array 
+export async function storeMetadata(json, imageContent) {
+  json.image = new File(imageContent, 'image.jpg', { type: 'image/jpg' });
+  const metadata = await client.store({
+    name: 'Pinpie',
+    description: 'Pin is not delicious beef!',
+    image: new File([/* data */], 'pinpie.jpg', { type: 'image/jpg' })
+  });
+  return metadata.url;
 }
