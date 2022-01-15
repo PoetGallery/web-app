e494b88d54fb9f295340b363b0f036fb55bd0955
Milena Monova <mimonova13@gmail.com>
2022-01-16 00:09:55 +0200

    registration connected to the backend, filecoin and added the copy

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -5,7 +5,7 @@ import {Grommet, Main} from 'grommet';
 function MainLayout(props) {
   return (
       <Grommet theme={theme}>
-        <Main background={{color: '#5867a8'}}>
+        <Main background={{color: '#001626'}}>
           {props.children}
         </Main>
       </Grommet>
